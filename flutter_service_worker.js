061ee43d374e8020857088adecb51946d2ffca4b
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2022-09-29 09:30:02 +0700

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "16a9a03781479eda787836a3fc96c7c3",
-"index.html": "7a7ab9a92455ae0f4f693539fcde848c",
-"/": "7a7ab9a92455ae0f4f693539fcde848c",
-"main.dart.js": "dc32b1085b9470986ff00aa5bfd16bd9",
-"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"index.html": "ebac12fedcabc317373e8653f21836ca",
+"/": "ebac12fedcabc317373e8653f21836ca",
+"main.dart.js": "049c159db0de3b0f718f7e0e4e2c9e17",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -26,11 +25,7 @@ const RESOURCES = {
 "assets/assets/icons/icon-notifications.png": "01e90e91bd50b2eb166784bac884b7e3",
 "assets/assets/icons/icon-correct@2x-green.png": "70325b6ace4a1fb08f6cdae73f72bb74",
 "assets/assets/icons/icon-caution@3x.png": "8f984d63371c3c065a6600c8a3ce4610",
-"assets/assets/icons/icon-error@3x.png": "e5b60c16a6694859a9a342e66dc09609",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
+"assets/assets/icons/icon-error@3x.png": "e5b60c16a6694859a9a342e66dc09609"
 };
 
 // The application shell files that are downloaded before a service worker can
